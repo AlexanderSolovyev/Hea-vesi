8ce53adb2bbfd7ea329796dec37b806645ea9778
AlexanderSolovyev <1430@list.ru>
2017-12-22 17:05:48 +0300

    please wait loader

--- src/pages/order/order.ts
+++ src/pages/order/order.ts
@@ -1,8 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import { NavController, NavParams, AlertController } from 'ionic-angular';
+import {NavController, NavParams, AlertController, LoadingController} from 'ionic-angular';
 import {OkPage} from "../ok/ok";
 import {StorageService} from "../storage.service";
-import {HttpErrorResponse} from "@angular/common/http";
 
 /**
  * Generated class for the OrderPage page.
@@ -20,7 +19,8 @@ export class OrderPage implements OnInit{
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               private storageservice: StorageService,
-              private alertCtrl: AlertController) {
+              private alertCtrl: AlertController,
+              public loadingCtrl: LoadingController) {
   }
 
   active: boolean=false;
@@ -46,25 +46,24 @@ export class OrderPage implements OnInit{
     this.data=this.storageservice.data;
   }
   goToOk(){
-    console.log(this.data,this.order);
+    let loading = this.loadingCtrl.create({
+      content: 'Please wait...'
+    });
+
+    loading.present();
     this.storageservice.sendOrder()
-      .subscribe(json => {
+      .subscribe(
+        json => {
           console.log(json);
+          loading.dismiss();
           this.navCtrl.push(OkPage);
     },
-        (err: HttpErrorResponse) => {
-          if (err.error instanceof Error) {
-            // A client-side or network error occurred. Handle it accordingly.
-            console.log('An error occurred:', err.error.message);
-          } else {
-            // The backend returned an unsuccessful response code.
-            // The response body may contain clues as to what went wrong,
-            console.log(`Backend returned code ${err.status}, body was: ${err.error}`)
-          };
-          this.errorAlert("Please try again later");
-        });
+        err => {
+          loading.dismiss();
+          this.errorAlert("Please try again later")}
 
-  }
+
+      )};
   decReturnedBottle(){
     if (this.order.returnedBottles > 0) {
       this.order.returnedBottles = this.order.returnedBottles - 1;
